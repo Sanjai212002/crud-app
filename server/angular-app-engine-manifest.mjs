a4093c0a85b29a18279fcbd8a2a4e3d3a346c8da
Sanjaykumar <sanjaikumarvr46@gmail.com>
2025-06-24 10:38:05 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/crud-app',
+  basePath: 'https://sanjai212002.github.io/crud-app',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/crud-app/',
+  baseHref: 'https://sanjai212002.github.io/crud-app/',
   locale: undefined,
   routes: [
   {
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5209, hash: '0f9120c10784103ca87cf5f277a51216ecd4d355cc29cc9b7c015dce68949ddc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1181, hash: '2747dd851184a9e1fff48fb97c4b6d2e2e8615a12ddd3bdb4b6eb59b653bd064', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 18212, hash: '85bc3a4bb095dc7d30bc4f109f4fe4fe687129b0042362239a93e75891c3191c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5239, hash: '6e6c1bbc19e7bc2fa93070b21a4c13f76a0463f7e253f02c9939eb9fd005411a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1211, hash: 'bd530c8460e4968de37dbe6f7ee39ffdf47eb59d9484ee93af2ce235886581ac', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 18242, hash: 'cfa77429e7acfc449e11dfedf95e8a2cc908c05a9504dd02b52d6f11f986de66', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-BVJQD57C.css': {size: 230873, hash: 'YU+im7r2LDs', text: () => import('./assets-chunks/styles-BVJQD57C_css.mjs').then(m => m.default)}
   },
 };
